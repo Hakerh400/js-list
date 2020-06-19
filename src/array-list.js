cee9222ff6b4f288356f35b908ce115c5da51112
Thomas <hakerh403@gmail.com>
2020-06-19 04:34:06 +0200

    commit

--- src/array-list.js
+++ src/array-list.js
@@ -7,6 +7,14 @@ const List = require('./list');
 const ListNode = require('./list-node');
 
 class ArrayList extends List{
+  constructor(arr=null){
+    super();
+
+    if(arr !== null)
+      for(const elem of arr)
+        this.push(elem);
+  }
+
   unshift(elem){ super.unshift(new ListNode(this, elem)); }
   push(elem){ super.push(new ListNode(this, elem)); }
   shift(){ return super.shift().val; }
